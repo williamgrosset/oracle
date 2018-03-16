4023c28b4acc9e9080fe3739899314d995f4dc14
William Grosset <williamhgrosset@gmail.com>
2018-03-15 23:05:56 -0700

    Dynamic buffer size

--- parts.c
+++ parts.c
@@ -8,7 +8,8 @@ void diskget(int argc, char* argv[]) {}
 void diskput(int argc, char* argv[]) {}
 
 int main(int argc, char* argv[]) {
-  unsigned char buffer[1024];
+  unsigned char *buffer;
+  size_t size;
   FILE *fp;
 
   fp = fopen(argv[1], "r");
@@ -17,13 +18,18 @@ int main(int argc, char* argv[]) {
     return(EXIT_FAILURE);
   }
 
-  fread(buffer, sizeof(buffer), 1, fp);
+  fseek(fp, 0, SEEK_END);
+  size = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+  buffer = malloc(size);
+  fread(buffer, sizeof *buffer, size, fp);
 
   int i;
-  for(i = 0; i < 1024; i++) {
-    printf("%x ", buffer[i]); // prints a series of bytes
+  for(i = 0; i < size; i++) {
+    printf("%02x ", buffer[i]); // prints a series of bytes
   }
 
   fclose(fp);
+  free(buffer);
   return(EXIT_SUCCESS);
 }
