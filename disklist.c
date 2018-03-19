78c55b215a6f75eef3b490ea00d39ca035940212
William Grosset <williamhgrosset@gmail.com>
2018-03-19 14:42:08 -0700

    Nit: temp remove diskget/diskput

--- disklist.c
+++ disklist.c
@@ -10,25 +10,12 @@ int main(int argc, char* argv[]) {
   }
 
   unsigned char* buffer = disk_buffer(argv[1]);
-  int block_size = get_super_block_info(buffer, 8, 10);
-  int block_count = get_super_block_info(buffer, 10, 14);
-  int FAT_start = get_super_block_info(buffer, 14, 18);
-  int FAT_blocks = get_super_block_info(buffer, 18, 22);
   int root_dir_start = get_super_block_info(buffer, 22, 26);
   int root_dir_blocks = get_super_block_info(buffer, 26, 30);
 
   printf("Super block information:\n");
-  printf("Block size: %i\n", block_size);
-  printf("Block count: %i\n", block_count);
-  printf("FAT starts: %i\n", FAT_start);
-  printf("FAT blocks: %i\n", FAT_blocks);
   printf("Root directory start: %i\n", root_dir_start);
   printf("Root directory blocks: %i\n\n", root_dir_blocks);
-
-  printf("FAT information:\n");
-  printf("Free Blocks:\n");
-  printf("Reserved Blocks:\n");
-  printf("Allocated Blocks:\n");
 
   return(EXIT_SUCCESS);
 }
