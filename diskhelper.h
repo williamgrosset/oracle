83274a535e90bfe310cc847c2a64d35222219fc2
William Grosset <williamhgrosset@gmail.com>
2018-03-23 11:37:23 -0700

    Add structs; remove unused methods

--- diskhelper.h
+++ diskhelper.h
@@ -1,3 +1,4 @@
+// Super block
 struct __attribute__((__packed__)) superblock_t {
   uint8_t fs_id [8];
   uint16_t block_size;
@@ -8,5 +9,24 @@ struct __attribute__((__packed__)) superblock_t {
   uint32_t root_dir_block_count;
 };
 
-unsigned char* disk_buffer(char* file);
-int get_super_block_info(unsigned char* buffer, int start, int end);
+// Time and date entry
+struct __attribute__((__packed__)) dir_entry_timedate_t {
+  uint16_t year;
+  uint8_t month;
+  uint8_t day;
+  uint8_t hour;
+  uint8_t minute;
+  uint8_t second;
+};
+
+// Directory entry
+struct __attribute__((__packed__)) dir_entry_t {
+  uint8_t status;
+  uint32_t starting_block;
+  uint32_t block_count;
+  uint32_t size;
+  struct dir_entry_timedate_t modify_time;
+  struct dir_entry_timedate_t create_time;
+  uint8_t filename[31];
+  uint8_t unused[6];
+};
