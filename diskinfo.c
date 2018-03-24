2a241a49a9558ddce7c09c8ba00ac0205792fd14
William Grosset <williamhgrosset@gmail.com>
2018-03-23 22:58:58 -0700

    Diskinfo clean-up

--- diskinfo.c
+++ diskinfo.c
@@ -6,9 +6,34 @@
 #include <sys/stat.h>
 #include "diskhelper.h"
 
-int get_fat_free_block_count() { return 0; }
-int get_fat_reserv_block_count() { return 0; }
-int get_fat_alloc_block_count() { return 0; }
+int get_fat_free_block_count(char* address, int fat_start, int block_size, int block_count) {
+  // TODO: remove duplicated loop logic
+  // TODO: audit calc -> block_count (6400) * 4 + block_size (512)
+  int fat_free_blocks = 0;
+  int i;
+  for (i = 0; i < (block_count * 4 + block_size); i += 4) {
+    int block = 0;
+    memcpy(&block, address + ((fat_start - 1) * block_size + i), 4);
+    if (htonl(block) == 0) fat_free_blocks++;
+  }
+  return fat_free_blocks;
+}
+
+int get_fat_reserv_block_count(char* address, int fat_start, int block_size, int block_count) {
+  // TODO: audit calc -> block_count (6400) * 4 + block_size (512)
+  int fat_reserv_blocks = 0;
+  int i;
+  for (i = 0; i < (block_count * 4 + block_size); i += 4) {
+    int block = 0;
+    memcpy(&block, address + ((fat_start - 1) * block_size + i), 4);
+    if (htonl(block) == 1) fat_reserv_blocks++;
+  }
+  return fat_reserv_blocks;
+}
+
+int get_fat_alloc_block_count(int block_count, int fat_free_blocks, int fat_reserv_blocks) {
+  return block_count - (fat_free_blocks + fat_reserv_blocks);
+}
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -16,31 +41,16 @@ int main(int argc, char* argv[]) {
     return(EXIT_FAILURE);
   }
 
-  struct stat buffer;
   int fd = open(argv[1], O_RDWR);
+  struct stat buffer;
   char* address = mmap(NULL, buffer.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   struct superblock_t *superblock = address;
   uint16_t block_size = htons(superblock->block_size);
   uint32_t block_count = htonl(superblock->file_system_block_count);
   uint32_t fat_start = htonl(superblock->fat_start_block);
-
-  int free_block = 0;
-  int reserv_block = 0;
-  int alloc_block = 0;
-  int i;
-  // TODO: audit calc -> block_count (6400) * 4 + block_size (512)
-  for (i = 0; i < (block_count * 4 + block_size); i += 4) {
-    int block = 0;
-    memcpy(&block, address + ((fat_start - 1) * block_size + i), 4);
-    if (htonl(block) == 0) free_block++;
-    if (htonl(block) == 1) reserv_block++;
-  }
-  printf("Free block:\n");
-  printf("%i\n", free_block);
-  printf("Reserve block:\n");
-  printf("%i\n", reserv_block);
-  printf("Alloc block:\n");
-  printf("%i\n", block_count - (free_block + reserv_block));
+  int fat_free_blocks = get_fat_free_block_count(address, fat_start, block_size, block_count);
+  int fat_reserv_blocks = get_fat_reserv_block_count(address, fat_start, block_size, block_count);
+  int fat_alloc_blocks = get_fat_alloc_block_count(block_count, fat_free_blocks, fat_reserv_blocks);
 
   printf("Super block information:\n");
   printf("Block size: %d\n", htons(superblock->block_size));
@@ -49,11 +59,10 @@ int main(int argc, char* argv[]) {
   printf("FAT blocks: %d\n", htonl(superblock->fat_block_count));
   printf("Root directory start: %d\n", htonl(superblock->root_dir_start_block));
   printf("Root directory blocks: %d\n\n", htonl(superblock->root_dir_block_count));
-
   printf("FAT information:\n");
-  printf("Free Blocks:\n");
-  printf("Reserved Blocks:\n");
-  printf("Allocated Blocks:\n");
+  printf("Free Blocks: %i\n", fat_free_blocks);
+  printf("Reserved Blocks: %i\n", fat_reserv_blocks);
+  printf("Allocated Blocks: %i\n", fat_alloc_blocks);
 
   return(EXIT_SUCCESS);
 }
